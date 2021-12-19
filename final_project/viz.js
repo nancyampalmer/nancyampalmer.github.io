e869757f8ce73fa527ea6d92e011a345946dadf0
nancyampalmer <nancy.am.palmer@gmail.com>
2021-12-19 09:39:26 -0500

    Adding rank

--- final_project/viz.js
+++ final_project/viz.js
@@ -1,12 +1,3 @@
-
-// add axes (y axis = bookname, author + rank? x-axis = weeks on list)
-// add chart title (Top 5 Bestselling Hardcover Nonfiction Books: x weeks until/since x election)
-// tooltip on hover --> stop function, book description appears on right side
-// add button for both years (upon selecting year, function runs --> bars enter/update/exit by week of that corresponding year
-//         OR is it just a slider?)
-
-// add 2020 data
-
 var height = 400;
 var width = 700;
 
@@ -20,15 +11,17 @@ var margin = {
 var chartHeight = height - margin.top - margin.bottom;
 var chartWidth = width - margin.left - margin.right;
 var chartBottom = height - margin.bottom;
-var authorTop = margin.top + 30
-var titleTop = margin.top + 50;
+var authorHeight = margin.top + 33;
+var titleHeight = margin.top + 50;
+
+
 
 var svg = d3.select("#chart")
     .attr("height", height)
     .attr("width", width);
 
 svg.append("text")
-    .attr("transform", "translate(" + (chartWidth/2 + margin.left) + "," + (chartBottom + 40) + ")")
+    .attr("transform", "translate(" + chartWidth + "," + (chartBottom + 40) + ")")
     .attr("class", "xAxisLabel")
     .style("text-anchor", "middle")
     .text("weeks on the list");
@@ -54,11 +47,13 @@ function drawBars(dataArray, maxWeeks) {
         .range([0,chartHeight])
         .paddingInner(0.1)
         .paddingOuter(0.2);
-    
-    
-    
+
     // x-axis
-    var xAxis = svg.select("#x").call(d3.axisBottom(xScale).tickSize(-chartHeight)).attr("transform", "translate(" + margin.left + ", " + chartBottom + ")");
+    var xAxis = svg.select("#x")
+        .call(d3.axisBottom(xScale)
+        .tickSize(-chartHeight))
+        .attr("transform", "translate(" + margin.left + ", " + chartBottom + ")");
+    
     xAxis.select(".domain").remove();
     
     // y-axis
@@ -66,13 +61,10 @@ function drawBars(dataArray, maxWeeks) {
 
     svg.select("#y")
         .call(yAxis)
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+        .attr("transform", "translate(15, " + margin.top + ")")
         .selectAll(".domain, .tick line")
             .remove();
 
-
-    // svg.select("#y domain").remove();
-    // yAxis.select(".domain").remove();
     
     // bind data + bars
     var bars = svg.select("#marks").selectAll(".mark")
@@ -80,7 +72,7 @@ function drawBars(dataArray, maxWeeks) {
         return d.title;
     });
 
-    // // bars enter
+    // bars enter
     bars.enter().append("rect")
         .attr("class", "mark")
         .attr("fill", "white")
@@ -119,7 +111,7 @@ function drawBars(dataArray, maxWeeks) {
             .attr("y", function (d) {
                 return barHeight (d.rank);
             })
-            .attr("transform", "translate(0, " + authorTop + ")")
+            .attr("transform", "translate(15, " + authorHeight + ")")
             .style('fill', 'white')
             .text(function(d) {
                 return d.author
@@ -145,7 +137,7 @@ function drawBars(dataArray, maxWeeks) {
             .attr("y", function(d) {
                 return barHeight(d.rank)
             })
-            .attr("transform", "translate(0, " + titleTop + ")")
+            .attr("transform", "translate(15, " + titleHeight + ")")
             .style('fill', 'white')
             .text(function(d){
                 return d.title
@@ -158,6 +150,7 @@ function drawBars(dataArray, maxWeeks) {
         
         titles.exit()
             .remove();
+        
 }
 
 
